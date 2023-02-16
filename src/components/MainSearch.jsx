25f161cd7f34c805f93d44b5c43396cdb32ee7de
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-16 16:49:59 +0100

    removed unused component in mainsearch

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Job from "./Job";
 import { useSelector } from "react-redux";
