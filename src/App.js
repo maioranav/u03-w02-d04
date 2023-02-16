9dbfe8e3506257170c7201fff53bb448a36c0f84
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-16 14:42:22 +0100

    Initial Commit, Redux Install

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MainSearch from './components/MainSearch'
+import CompanySearchResults from './components/CompanySearchResults'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Favourites } from './components/Favourites'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainSearch />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/:companyName" element={<CompanySearchResults />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Favourites.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export const Favourites = (props) => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={10} className="mx-auto my-3">
+          <h1>Sono la pagina favourites</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
