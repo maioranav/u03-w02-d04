8936c25bef7abe6e241c375bb2dad9f0be621298
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-16 15:47:50 +0100

    installed reducers for redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,23 @@
+const initialState = {
+   //IL NOSTRO STATO INIZIALE
+   favs: []
+}
+
+const mainReducer = (state = initialState, action) => {
+
+
+   //eseguo qualcosa per ogni azione.tipo del reducer interpellato
+
+   switch (action.type) {
+      case "ADDFAV":
+         return { ...state, favs: [...state.favs, action.payload] };
+      case "DELETEFAV":
+         return { ...state, favs: state.favs.filter((_, i) => i !== action.payload) };
+      default:
+         return state;
+   }
+
+
+}
+
+export default mainReducer
