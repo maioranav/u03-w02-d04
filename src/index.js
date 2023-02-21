635a8288b70da70c826522adc0d7053e89b84c42
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-21 13:37:45 +0100

    added redux persist encrypt

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
    <Provider store={store}>
+      <PersistGate persistor={persistor} />
       <App />
    </Provider>
 )
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,15 +1,33 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import favsReducer from "../reducers/favourites";
 import jobsReducer from "../reducers/jobs";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+//do una configurazione alla persistenza (cosa gestire e dove renderlo persistente)
+const persistConfig = {
+   key: `root`,
+   storage: storage,
+   whitelist: ["favs"],
+   transforms: [encryptTransform({ secretKey: process.env.REACT_APP_PERSISTKEY })]
+}
 
 const rootReducer = combineReducers({
    favs: favsReducer,
    jobs: jobsReducer,
 });
 
+//rendo persistente (config, reducers)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = configureStore({
-   reducer: rootReducer,
+
+export const store = configureStore({
+   reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         serializableCheck: false
+      }),
 })
 
-export default store;
+export const persistor = persistStore(store)
